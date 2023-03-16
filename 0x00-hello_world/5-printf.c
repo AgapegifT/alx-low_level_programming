82787dcb71926ce6f91709b905b3cab6ed1a7eaa
AgapegifT <agapegift223@gmail.com>
2023-03-16 09:21:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - a C program that prints exactly aline using printf
+ * main - a C program that prints exactly a line using printf
  * Return: 0 (success)
  */
-int main(void){       
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+ 
