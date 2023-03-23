3699662e63f65d4a60f5d407f70bde38db0ece6a
AgapegifT <agapegift223@gmail.com>
2023-03-23 02:06:53 -0700

    2-mul.c

---
+++ 0x03-debugging/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @x: the first number of multiply
+ * @y: the second number of multiply by first number
+ * Return: the result of multiplication between x param and y param
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
