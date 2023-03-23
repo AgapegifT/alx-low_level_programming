101eb01307935ed50db8bfd4e8d536d7e4d7914b
AgapegifT <agapegift223@gmail.com>
2023-03-23 04:13:58 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9, followed by a new line
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
